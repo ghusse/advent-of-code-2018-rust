cc776badbe9fe0f13bf1a0e15c9ef5d7eae32a24
Guillaume Gautreau <guillaume.gautreau@afidium.com>
2018-12-01 10:53:08 +0100

    Day 1

---
+++ src/day_1/mod.rs
@@ -0,0 +1,43 @@
+use std::io::BufReader;
+use std::io::BufRead;
+use std::fs::File;
+use std::iter::Iterator;
+use std::collections::HashSet;
+
+pub fn solve() {
+   solve1();
+   solve2();
+}
+
+fn solve1(){
+    let sum: i64 = get_frequency_changes()
+        .iter()
+        .sum();
+
+    println!("Result day1 1/2 {}", sum);
+}
+
+fn solve2(){
+    let changes: Vec<i64> = get_frequency_changes();
+    let mut encountered: HashSet<i64> = HashSet::new();
+    let mut index = 0;
+    let mut sum = 0;
+
+    while !encountered.contains(&sum) {
+        encountered.insert(sum);
+        sum += changes[index];
+        index = (index + 1) % changes.len();
+    }
+
+    println!("Result day1 2/2 {}", sum);
+
+}
+
+fn get_frequency_changes() -> Vec<i64>{
+    let input_file = File::open("/home/guillaume/Perso/advent/src/day_1/input.txt").expect("file not found");
+
+    return BufReader::new(input_file).lines()
+        .map(|line| line.expect("error readding the line"))
+        .map(|line| line.parse::<i64>().unwrap())
+        .collect();
+}
